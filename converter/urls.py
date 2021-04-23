5592f7e36906daaf548264c33496f3c7eba23030
Administrator <kathleen@zayls.com>
2021-04-23 19:20:05 +0800

    pasted stories url

--- converter/urls.py
+++ converter/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.start, name='start'),
     path('start/', views.start, name='start'),
     path('main/', views.main, name='main'),
+    path('stories/', views.stories, name='stories'),
     path('stories/<uuid:id>/annotate/', views.annotate, name='annotate'),
 ]
