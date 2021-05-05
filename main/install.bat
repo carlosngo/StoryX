b4ffba256cd7db3836a6e4aeb97ecd612d23a661
Carlos <carlos_ngo@dlsu.edu.ph>
2021-05-05 18:18:41 +0800

    hotfix for installation batch file

--- main/install.bat
+++ main/install.bat
@@ -2,4 +2,6 @@ py -m venv env
 call ".\env\Scripts\activate.bat"
 py -m pip install -r requirements.txt
 py -m spacy download en_core_web_sm
+py manage.py makemigrations
+py manage.py migrate
 deactivate
